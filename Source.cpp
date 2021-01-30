9be5cc57aaf54b879111e1095614538a754f46ed
dleot <airflow0@gmail.com>
2021-01-30 01:07:53 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+/*
+0|1|2
+3|4|5
+6|7|8
+
+3x3 matrix
+
+*/
+
+
+//initalize the board; 
+int board[9] =
+{
+	-1,-1,-1,
+	-1,-1,-1,
+	-1,-1,-1
+};
+
+int getBoardSize() {
+	return sizeof(board) / sizeof(*board);
+}
+void printBoard()
+{
+	for (int i = 0; i < getBoardSize(); i++) {
+		cout << board[i] << endl;
+	}
+}
+
+
+int main()
+{
+	
+	printBoard();
+
+	/*while (true)
+	{
+		printBoard();
+		char input;
+		cout << "Press F to end game" << endl;
+		cin >> input;
+		if (input == 'f')
+		{
+			break;
+		}
+
+	}
+	return 0;*/
+}
