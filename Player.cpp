859aae53447e8d9ebc44de8154fb749febde56a1
dleot <airflow0@gmail.com>
2021-01-30 15:50:24 -0500

    +Initial Player Setup?

---
+++ Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+
+using namespace player;
+
+std::string Player::getName()
+{
+	return this->name;
+}
+
+void Player::save()
+{
+
+}
--- Player.h
+++ Player.h
@@ -1,1 +1,37 @@
+#include <string>
 #pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+namespace player
+{
+	
+	class Player
+	{
+	public:
+		std::string getName();
+		void setName();
+		void save();
+		void increaseWins();
+		void increaseLose();
+		void increaseMoves();
+		void load();
+		
+		Player(std::string name)
+		{
+			this->name = name;
+			
+		}
+
+
+	private:
+		std::string name;
+		int wins, lose, moves;
+
+	};
+}
+
+
+
+#endif
